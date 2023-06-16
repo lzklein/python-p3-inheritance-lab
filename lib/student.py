e3e67e1a6a801f5d854b24fc09968f26a13a1a8d
Louis Klein <louisklein71@gmail.com>
2023-06-16 13:31:57 -0700

    Test

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,8 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name, knowledge = None):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge if knowledge is not None else []
+    def learn(self, new_knowledge):
+        self.knowledge.append(new_knowledge)
